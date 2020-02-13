c2590eb59fceccbb966ed52b05c469eddd4241d5
thor <thor@cphbusiness.dk>
2020-02-13 10:38:33 +0100

    first

--- ScannerOpg.java
+++ ScannerOpg.java
@@ -19,19 +19,8 @@ public class ScannerOpg {
        */
       Scanner myScan = new Scanner(System.in);
       System.out.println("Hvor gammel?");
-      int age = myScan.nextInt();
-      System.out.println("Hvad hedder du ?");
-      myScan.nextLine();
-      String name = myScan.nextLine();
-      System.out.println("Postnr ?");
-      int postal = myScan.nextInt();
-      System.out.println("Hvad er din hovedinteresse?");
-      myScan.nextLine();
-      String interesse = myScan.nextLine();
-      System.out.println("Bor du i byen?");
-      boolean liveCity = myScan.nextBoolean();
 
-      System.out.printf("Det er %b at %s bor i byen%n",liveCity,name);
+      //System.out.printf("Det er %b at %s bor i byen%n",liveCity,name);
       
       // Definér en konstant som er generationsårene
       // udregn generationen
@@ -39,10 +28,7 @@ public class ScannerOpg {
       // hvor mange år der er til at man
       // skifter til næste generation
 
-      final int generationPeriode = 30;
-      int generation = age/generationPeriode;
-      int rest = age%generationPeriode;
-      System.out.printf("Du tilhører %d generation og har %d år inden skift%n",generation,(generationPeriode - rest));
+      //System.out.printf("Du tilhører %d generation og har %d år inden skift%n",generation,(generationPeriode - rest));
       // Prompt brugeren for hvor mange minutter siden sidst (over 100) 
       // udregn timer og minutter
       
