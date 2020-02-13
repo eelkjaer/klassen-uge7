408a827951e62a3df309987decf693d3e77a6003
thor <thor@cphbusiness.dk>
2020-02-13 23:07:57 +0100

    first

--- BasicElements.java
+++ BasicElements.java
@@ -5,8 +5,8 @@ public class BasicElements {
 
     int a = 45 + 10.5;//vha typecast
     String navn = 1 + 2 + 3;//vha "
-    10 = 8 - 3 * 5;//vha parenteser
-    30 = 10.5 + 20.6;//vha typecast 
+    int x = 8 - 3 * 5;//vha parenteser så x = 10
+    int y = 10.5 + 20.6;//vha typecast så y = 30
   }
 }
 
