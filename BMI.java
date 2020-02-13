099a44b495daf77eaac219e2b89d4a9296278773
thor <thor@cphbusiness.dk>
2020-02-13 23:04:13 +0100

    first

---
+++ BMI.java
@@ -0,0 +1,18 @@
+public class BMI {
+  public static void main(String[] args) {
+    // lav en scanner
+    // bed om højde
+    // hent til scanner
+    // bed om vægt
+    // hent til scanner
+    // bed om navn
+    // hent til scanner
+    // OBS: pas på scanner-buggen
+    // gem format-linjen i en streng-variabel for sig
+    // udregn bmi og udskriv vha printf(format,navn,bmi)
+    
+  
+  }
+
+
+}
