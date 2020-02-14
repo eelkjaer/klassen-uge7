91f90eec735385563a109530e4684e6da6e16270
Emil Elkjær Nielsen <cph-en93@cphbusiness.dk>
2020-02-14 09:47:39 +0100

    Done

--- BMI.java
+++ BMI.java
@@ -2,18 +2,6 @@ import java.util.Scanner;
 
 public class BMI {
     public static void main(String[] args) {
-    /*
-    lav en scanner
-    bed om højde
-    hent til scanner
-    bed om vægt
-    hent til scanner
-    bed om navn
-    hent til scanner
-    OBS: pas på scanner-buggen
-    gem format-linjen i en streng-variabel for sig
-    udregn bmi og udskriv vha printf(format,navn,bmi)
-    */
 
     // Init klasser
     Scanner inputScanner = new Scanner(System.in);
@@ -37,14 +25,11 @@ public class BMI {
     /* 
     Beregner BMI.
     Formel: Vægt / (Højde^2)
-    Ganger med 10000 for at kunne gøre det i cm fremfor meter
     */
-    bmi = Math.pow(height, 2);
-    bmi = weight / bmi;
-    bmi = Math.round(bmi * 10000);
+    bmi = weight / Math.pow(height/100f, 2);
 
     // Printer formatteret String
-    System.out.printf("Hej %s. Med en kampvægt på %dkg og højde på %dcm, er din BMI: %d", name, weight, height, (int)bmi);
+    System.out.printf("Hej %s. Med en kampvægt på %dkg og højde på %dcm, er din BMI: %.2f", name, weight, height, bmi);
 
     }
 }
